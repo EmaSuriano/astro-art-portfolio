0a7559ca56b27a426dbac67aaa8b9ec0e3f2a5df
Emanuel Suriano <emanuel.suriano@revolut.com>
2022-09-29 12:23:51 +0200

    add new lightbox

---
+++ src/consts.ts
@@ -0,0 +1,21 @@
+export const ROUTES = [
+  {
+    href: '/',
+    label: 'Art',
+  },
+  {
+    href: '/about',
+    label: 'About',
+  },
+] as const;
+
+export const SOCIAL = [
+  {
+    href: 'https://twitter.com/EmaSuriano',
+    icon: 'twitter',
+  },
+  {
+    href: 'https://www.linkedin.com/in/emanuel-suriano/',
+    icon: 'linkedIn',
+  },
+] as const;
