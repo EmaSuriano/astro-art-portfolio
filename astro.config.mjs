ad4df811d1f69d2ca8f7ec3337b3f493326aaf91
Emanuel Suriano <emanuel.suriano@revolut.com>
2022-12-02 11:04:57 +0100

    feat: adding astro-compress for performance improvement

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import { astroImageTools } from 'astro-imagetools';
-
 import tailwind from '@astrojs/tailwind';
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroImageTools, tailwind()],
+  integrations: [astroImageTools, tailwind(), compress()]
 });
