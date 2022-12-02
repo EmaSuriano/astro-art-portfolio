7511ec7132865b8324d898ad55a84d128640bdab
Emanuel Suriano <emanuel.suriano@revolut.com>
2022-12-02 11:16:30 +0100

    feat: add support for webp

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import { astroImageTools } from 'astro-imagetools';
 import tailwind from '@astrojs/tailwind';
 
-import compress from "astro-compress";
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroImageTools, tailwind(), compress()]
+  loading: 'lazy',
+  preload: 'webp',
+  integrations: [astroImageTools, tailwind(), compress()],
 });
