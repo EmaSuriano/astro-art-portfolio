e12e89ca28884c9a1b7b776209e954b58448abc5
Emanuel Suriano <emanuel.suriano@revolut.com>
2025-01-19 17:55:00 +0100

    regenerate yarn and cleanup consolelog

--- src/load-gallery.js
+++ src/load-gallery.js
@@ -1,34 +1,33 @@
-import PhotoSwipeLightbox from 'photoswipe/lightbox';
-import 'photoswipe/style.css';
+import PhotoSwipeLightbox from "photoswipe/lightbox";
+import "photoswipe/style.css";
 
 const lightbox = new PhotoSwipeLightbox({
-  gallery: '#my-gallery',
-  children: 'a',
-  pswpModule: () => import('photoswipe'),
+  gallery: "#my-gallery",
+  children: "a",
+  pswpModule: () => import("photoswipe"),
 });
 
-lightbox.on('uiRegister', function () {
+lightbox.on("uiRegister", function () {
   lightbox.pswp.ui.registerElement({
-    name: 'download-button',
+    name: "download-button",
     order: 8,
     isButton: true,
-    tagName: 'a',
+    tagName: "a",
 
     // SVG with outline
     html: {
       isCustomSVG: true,
       inner:
         '<path d="M20.5 14.3 17.1 18V10h-2.2v7.9l-3.4-3.6L10 16l6 6.1 6-6.1ZM23 23H9v2h14Z" id="pswp__icn-download"/>',
-      outlineID: 'pswp__icn-download',
+      outlineID: "pswp__icn-download",
     },
 
     onInit: (el, pswp) => {
-      el.setAttribute('download', '');
-      el.setAttribute('target', '_blank');
-      el.setAttribute('rel', 'noopener');
+      el.setAttribute("download", "");
+      el.setAttribute("target", "_blank");
+      el.setAttribute("rel", "noopener");
 
-      pswp.on('change', () => {
-        console.log('change');
+      pswp.on("change", () => {
         el.href = pswp.currSlide.data.src;
       });
     },
