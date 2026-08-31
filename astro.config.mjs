07a655cb0930d4c199e1a415e106c4c5f74ad5e7
Ema Suriano <emanuel.suriano@gmail.com>
2026-08-31 09:12:17 +0000

    ci: GitHub Pages PR previews

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  base: process.env.PAGES_BASE || "",
   integrations: [icon()],
   vite: {
     plugins: [tailwindcss()],
